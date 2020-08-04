cb923947e72674b940c876fb3cf466e14600f435
David Rose <doppler@gmail.com>
2020-08-04 15:54:38 -0400

    clean up face style

--- src/Windchart.tsx
+++ src/Windchart.tsx
@@ -48,51 +48,46 @@ export const Windchart: React.FC<Props> = ({ options, data, width, height }) => 
     >
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height={height} width={width}>
         <defs>
-          <path id="hash" d="M 250 3 L 256 29 L 262 3 Z" fill="black" />
+          <path id="hashmark" d="M 250 3 L 256 29 L 262 3 Z" className={styles.hashmark} />
           <line id="line" x1="256" y1="32" x2="256" y2="480" className={styles.line} />
-          <mask id="hash-mask">
-            <circle cx="256" cy="256" r="240" stroke="white" stroke-width="32px" />
-
-            <g>
-              <text x="243" y="30" fill="black" className={styles.text}>
-                N
-              </text>
-              <text x="483" y="269" fill="black" className={styles.text}>
-                E
-              </text>
-              <text x="243" y="509" fill="black" className={styles.text}>
-                S
-              </text>
-              <text x="4" y="269" fill="black" className={styles.text}>
-                W
-              </text>
-
-              <use href="#hash" transform="rotate(22.5, 256, 256)" />
-              <use href="#hash" transform="rotate(45, 256, 256)" />
-              <use href="#hash" transform="rotate(67.5, 256, 256)" />
-              <use href="#hash" transform="rotate(112.5, 256, 256)" />
-              <use href="#hash" transform="rotate(135, 256, 256)" />
-              <use href="#hash" transform="rotate(157.5, 256, 256)" />
-              <use href="#hash" transform="rotate(202.5, 256, 256)" />
-              <use href="#hash" transform="rotate(225, 256, 256)" />
-              <use href="#hash" transform="rotate(247.5, 256, 256)" />
-              <use href="#hash" transform="rotate(292.5, 256, 256)" />
-              <use href="#hash" transform="rotate(315, 256, 256)" />
-              <use href="#hash" transform="rotate(337.5, 256, 256)" />
-            </g>
-          </mask>
         </defs>
 
-        {/* <use href="#line" /> */}
+        <g className={styles.text}>
+          <text x="243" y="30">
+            N
+          </text>
+          <text x="483" y="269">
+            E
+          </text>
+          <text x="243" y="509">
+            S
+          </text>
+          <text x="4" y="269">
+            W
+          </text>
+        </g>
+        <g>
+          <use href="#hashmark" transform="rotate(22.5, 256, 256)" />
+          <use href="#hashmark" transform="rotate(45, 256, 256)" />
+          <use href="#hashmark" transform="rotate(67.5, 256, 256)" />
+          <use href="#hashmark" transform="rotate(112.5, 256, 256)" />
+          <use href="#hashmark" transform="rotate(135, 256, 256)" />
+          <use href="#hashmark" transform="rotate(157.5, 256, 256)" />
+          <use href="#hashmark" transform="rotate(202.5, 256, 256)" />
+          <use href="#hashmark" transform="rotate(225, 256, 256)" />
+          <use href="#hashmark" transform="rotate(247.5, 256, 256)" />
+          <use href="#hashmark" transform="rotate(292.5, 256, 256)" />
+          <use href="#hashmark" transform="rotate(315, 256, 256)" />
+          <use href="#hashmark" transform="rotate(337.5, 256, 256)" />
+        </g>
+
         <use href="#line" transform="rotate(22.5, 256, 256)" />
-        {/* <use href="#line" transform="rotate(45, 256, 256)" /> */}
         <use href="#line" transform="rotate(67.5, 256, 256)" />
-        {/* <use href="#line" transform="rotate(90, 256, 256)" /> */}
         <use href="#line" transform="rotate(112.5, 256, 256)" />
-        {/* <use href="#line" transform="rotate(135, 256, 256)" /> */}
         <use href="#line" transform="rotate(157.5, 256, 256)" />
 
-        <circle className={styles.face} id="face" cx="256" cy="256" r="240" mask="url(#hash-mask)" />
+        <circle cx={256} cy={256} r={220} style={{ fill: 'transparent', stroke: 'grey', strokeWidth: 1 }} />
+
         <g>
           {ringRadii.map(i => (
             //@ts-ignore
@@ -160,19 +155,22 @@ const getStyles = stylesFactory(theme => {
       top: 0;
       left: 0;
     `,
+    hashmark: css`
+      fill: grey;
+    `,
     face: css`
       fill: none;
       stroke: ${theme.palette.yellow};
       stroke-width: 32px;
     `,
     text: css`
-      fill: black;
+      fill: grey;
       font-weight: bold;
       font-family: 'Courier New', Courier, monospace;
       font-size: 3rem;
     `,
     line: css`
-      stroke: darkgrey;
+      stroke: grey;
       stroke-width: 1px;
     `,
     ring: css`
